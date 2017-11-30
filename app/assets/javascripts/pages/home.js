dd5274dbfe6b1a4031161a429235067f6ce36fa1
BingDarla <hbshuang@gmail.com>
2017-11-30 23:11:44 +1100

    local version

--- app/assets/javascripts/pages/home.js
+++ app/assets/javascripts/pages/home.js
@@ -4,43 +4,43 @@ var map = AmCharts.makeChart("chartdiv", {
   "map": "worldLow",
   "areas": [{
     "id": "AU",
-    "modalUrl": "https://pacific-waters-84155.herokuapp.com/countries/3",
+    "modalUrl": "http://localhost:3000/countries/3",
     "selectable": true
   }, {
     "id": "JP",
-    "modalUrl": "https://pacific-waters-84155.herokuapp.com/countries/2",
+    "modalUrl": "http://localhost:3000/countries/2",
     "selectable": true
   }, {
     "id": "CN",
-    "modalUrl": "https://pacific-waters-84155.herokuapp.com/countries/1",
+    "modalUrl": "http://localhost:3000/countries/1",
     "selectable": true
   },{
     "id": "BR",
-    "modalUrl": "https://pacific-waters-84155.herokuapp.com/countries/4",
+    "modalUrl": "http://localhost:3000/countries/4",
     "selectable": true
   },{
     "id": "CA",
-    "modalUrl": "https://pacific-waters-84155.herokuapp.com/countries/5",
+    "modalUrl": "http://localhost:3000/countries/5",
     "selectable": true
   },{
     "id": "US",
-    "modalUrl": "https://pacific-waters-84155.herokuapp.com/countries/6",
+    "modalUrl": "http://localhost:3000/countries/6",
     "selectable": true
   },{
     "id": "GL",
-    "modalUrl": "https://pacific-waters-84155.herokuapp.com/countries/7",
+    "modalUrl": "http://localhost:3000/countries/7",
     "selectable": true
   },{
     "id": "AR",
-    "modalUrl": "https://pacific-waters-84155.herokuapp.com/countries/8",
+    "modalUrl": "http://localhost:3000/countries/8",
     "selectable": true
   },{
     "id": "RU",
-    "modalUrl": "https://pacific-waters-84155.herokuapp.com/countries/9",
+    "modalUrl": "http://localhost:3000/countries/9",
     "selectable": true
   },{
     "id": "AO",
-    "modalUrl": "https://pacific-waters-84155.herokuapp.com/countries/10",
+    "modalUrl": "http://localhost:3000/countries/10",
     "selectable": true
   }]
 },
